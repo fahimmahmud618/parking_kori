9df42d46fba2b4b2cbbff3d616a575ecb7ddbd58
mehzabin-haque <bsse1233@iit.du.ac.bd>
2024-05-15 13:22:35 +0600

    printing test

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:parking_kori/printing/bluetooth.dart';
+// import 'package:parking_kori/printing/bluetooth.dart';
 import 'package:parking_kori/view/pages/flash_page.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:parking_kori/view/styles.dart';
 import 'package:parking_kori/view/pages/checkout.dart';
+import 'package:parking_kori/printing/test.dart';
 
 void main() async {
   await dotenv.load(fileName: "lib/.env");
@@ -15,6 +16,6 @@ void main() async {
       primaryColor: myred, 
     ),
     debugShowCheckedModeBanner: false,
-    home: FlashPage(),
+    home: PrintPage(),
   ));
 }
